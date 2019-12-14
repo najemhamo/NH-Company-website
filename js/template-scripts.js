7a32bb4b269d6f5a48ccc5117d8402b858013939
Najem Hamo <najemhamo25@gmail.com>
2019-12-14 05:12:56 +0100

    initial project files

---
+++ js/template-scripts.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function($) {                      
+  var owl = $('.carousel-default');
+  owl.owlCarousel({
+    nav: true,
+    dots: true,
+    items: 1,
+    loop: true,
+    navText: ["&#xe605","&#xe606"],
+    autoplay: true,
+    autoplayTimeout: 5000
+  });
+
+
+  var owl = $('.carousel-blocks');
+  owl.owlCarousel({
+    nav: true,
+    dots: false,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1
+      },
+      481: {
+        items: 3
+      },
+      769: {
+        items: 4
+      }
+    },
+    loop: true,
+    navText: ["&#xe605","&#xe606"],
+    autoplay: true,
+    autoplayTimeout: 5000
+  });
+  
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 20){  
+        $('.sticky').addClass("fixed");
+    }
+    else{
+        $('.sticky').removeClass("fixed");
+    }
+  });
+});
